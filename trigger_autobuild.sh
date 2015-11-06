33c15069dd0606182d66f422dba25d5ed461f657
David Brawand <dbrawand@nhs.net>
2015-11-06 14:41:18 +0000

    fixed tag update script

--- trigger_autobuild.sh
+++ trigger_autobuild.sh
@@ -2,6 +2,8 @@
 git tag -d flatdog && \
 	git push origin :refs/tags/flatdog && \
 	git push autobuild :refs/tags/flatdog && \
-	git tag flatdog && \
 	git push origin && \
-	git push autobuild
+	git push autobuild && \
+	git tag flatdog && \
+	git push origin --tags && \
+	git push autobuild --tags
